02e7f593b1292656b218b78c8e82613c93d747b1
Bruno <bruno@renostro.com>
2020-10-19 13:19:10 -0400

    second day of programming

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,24 +1,97 @@
 import React from 'react';
 import {Button,TextField,Paper} from '@material-ui/core';
 import {VictoryChart,VictoryScatter} from 'victory';
+import {Container,Tables} from './style';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+
+
+function createData(name:number, calories:number, fat:number, carbs:number, protein:number) {
+  return { name, calories, fat, carbs, protein };
+}
+
 const Dashboard:React.FC =() =>{
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+  
+ 
+  
+  const rows = [
+    createData(1, 159, 6.0, 24, 4.0),
+    createData(2, 237, 9.0, 37, 4.3),
+    createData(3, 262, 16.0, 24, 6.0),
+    createData(4, 305, 3.7, 67, 4.3),
+    createData(5, 356, 16.0, 49, 3.9),
+  ];
+  const classes = useStyles();
     return (
     <>
+    <Container>
     <Paper elevation={3}>
-    <h1>Ola mundo</h1>
+    <h1>Acompanhe sua evolução </h1>
     <form  noValidate autoComplete="off">
   
   <TextField id="outlined-basic" label="Peso em kg" variant="outlined" />
 </form>
         <Button color='primary' variant='contained'>Adicionar peso</Button>
+        <Tables>
+
+
+
+
+
+
+
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dia do mês</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+</Tables>
         <VictoryChart  height={200} width={200}
   style={{
     background: { fill: "lavender" }
   }}
 >
+
+  
   <VictoryScatter />
 </VictoryChart>
         </Paper>
+        </Container>
       </>  
         );
 }
---
+++ src/pages/dashboard/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container=styled.div`
+display:flex;
+justify-self:center;
+align-items:center;
+`;
+
+export const Tables=styled.div`
+
+
+`;
+
