41a6a2faeb6d0c1f98ea105baabf730e1cfb8076
Bruno <bruno@renostro.com>
2020-10-19 13:26:42 -0400

    some table alterations

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -27,11 +27,11 @@ const Dashboard:React.FC =() =>{
  
   
   const rows = [
-    createData(1, 159, 6.0, 24, 4.0),
-    createData(2, 237, 9.0, 37, 4.3),
-    createData(3, 262, 16.0, 24, 6.0),
-    createData(4, 305, 3.7, 67, 4.3),
-    createData(5, 356, 16.0, 49, 3.9),
+    createData(1, 74.6, 6.0, 24, 4.0),
+    createData(2, 75.3, 9.0, 37, 4.3),
+    createData(3, 77.5, 16.0, 24, 6.0),
+    createData(4, 73.2, 3.7, 67, 4.3),
+    createData(5, 71.4, 16.0, 49, 3.9),
   ];
   const classes = useStyles();
     return (
@@ -45,22 +45,13 @@ const Dashboard:React.FC =() =>{
 </form>
         <Button color='primary' variant='contained'>Adicionar peso</Button>
         <Tables>
-
-
-
-
-
-
-
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Dia do mês</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">Peso em KG</TableCell>
+            
           </TableRow>
         </TableHead>
         <TableBody>
@@ -70,9 +61,7 @@ const Dashboard:React.FC =() =>{
                 {row.name}
               </TableCell>
               <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+             
             </TableRow>
           ))}
         </TableBody>
